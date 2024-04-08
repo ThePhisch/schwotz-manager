edda1713adaa8f60a50df9c33573334647d5fa09
Anton Schwarz <anton@mendoza-schwarz.de>
2024-04-08 18:45:35 +0200

    Sort cards by date (i.e. enxtup)

--- frontend/schwotz-frontend/src/components/SortedCards.tsx
+++ frontend/schwotz-frontend/src/components/SortedCards.tsx
@@ -11,13 +11,18 @@ export default function SortedCards(props: {
 	const [tasklist, setTasklist] = useState<Task[]>([]);
 
 	useEffect(() => {
+		// unpack promise and sort by nextup (i.e. date)
 		props.tasklist().then((tasklist) => {
-			setTasklist(tasklist);
+			setTasklist(
+				tasklist.sort(
+					(a, b) => a.nextup > b.nextup ? 1 : -1
+				)
+			);
 		});
 	}, [tasklist])
 
 	return (
-		<div className={twMerge("grid grid-cols-1 gap-4")}>
+	<div className={twMerge("grid grid-cols-1 gap-2")}>
 			{tasklist.map((task) => {
 				return (
 					<Card
